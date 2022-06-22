ef31b2572fe2b25f89d08907362e2177fa57b52a
welinson <welinsonmarlon15@gmail.com>
2022-06-22 20:34:11 -0300

    products

---
+++ src/app/controllers/ProductControler.js
@@ -0,0 +1,22 @@
+import * as Yup from 'yup'
+
+class ProductControler{
+    async store(request,response){
+
+        const schema=Yup.object().shape({
+            name:Yup.string().required(),
+            price:Yup.number().required(),
+            category:Yup.string().required(),
+        })
+        try{
+            await schema.validateSync(request.body,{abortEarly:false})
+        }catch(err){
+            return response.status(400).json({error:err.errors})
+            }
+            return response.json({message:'oiii'})
+    }
+
+}
+
+
+export default new ProductControler()
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
+import multer from 'multer'
+import configmulter from '../src/config/multer'
 
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
+import ProductControler from "./app/controllers/ProductControler";
+
+const upload=multer(configmulter);
 const routes = new Router();
 
 routes.post("/users",UserController.store)
 routes.post("/session",SessionController.store)
+routes.post("/products",upload.single('file'),ProductControler.store)
 
 export default routes;
